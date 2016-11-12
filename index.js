a4c43da386eac3df27e985e9b4295b059a1c1372
Matthew-Smith <matthewb.smith88@gmail.com>
2016-11-11 20:17:12 -0500

    Got the Server sending different page based on url

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
-app.use(express.static(path.join(__dirname, '/public')));
+// Static serving
+app.use('/public', express.static(path.join(__dirname, '/public')));
 app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
+
+// Home Page
+app.use(/^\/$/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// Deck page as default
+app.use(/^\/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/deck.html'));
+});
 
 app.listen(app.get('port'), () => {
     mongoose.connect('mongodb://192.168.0.17:27017/deckinator');
