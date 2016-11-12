8ef95abc537d6e00a636a88155bc3bd3e6a63fcd
Sean Turnbull <sean.turnbull@gmail.com>
2016-11-12 09:11:47 -0500

    Clean up landing page

--- public/index.js
+++ public/index.js
@@ -1,17 +1,21 @@
-/*global angular*/
+/* global angular*/
 (function () {
     'use strict';
+
     const app = angular.module('deckitron', ['slug', 'ngMaterial']);
 
-    app.controller('DeckitronHome', ['$scope', '$window', 'slug', function ($scope, $window, slug) {
-        $scope.title = 'DeckitronHome';
-        $scope.onStart = function () {
-            var sluggedDeckName = $scope.deckName ? slug($scope.deckName) : null;
-            if (sluggedDeckName) {
-                $window.location.href = '/' + sluggedDeckName;
-            } else {
-                $scope.ErrMsg = "Invalid Deck Name";
-            }
-        };
-    }]);
-})();
+    app.controller('DeckitronHome', ['$scope', '$window', 'slug',
+        function ($scope, $window, slug) {
+            $scope.title = 'DeckitronHome';
+            $scope.onStart = function () {
+                const sluggedDeckName = $scope.deckName
+                    ? slug($scope.deckName)
+                    : null;
+                if (sluggedDeckName) {
+                    $window.location.href = '/' + sluggedDeckName;
+                } else {
+                    $scope.ErrMsg = 'Invalid Deck Name';
+                }
+            };
+        }]);
+}());
