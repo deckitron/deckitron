291b2a51d354b76765387b097ce99acfe93d7af2
Sean Turnbull <sean.turnbull@gmail.com>
2016-11-11 22:58:26 -0500

    Slugify Deck Name

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
 /*global angular*/
 (function () {
     'use strict';
-    const app = angular.module('deckitron', []);
+    const app = angular.module('deckitron', ['slug']);
 
-    app.controller('DeckitronHome', ['$scope', '$window', function ($scope, $window) {
+    app.controller('DeckitronHome', ['$scope', '$window', 'slug', function ($scope, $window, slug) {
         $scope.title = 'DeckitronHome';
         $scope.onStart = function () {
-            $window.location.href = '/' + $scope.deckName;
+            $window.location.href = '/' + slug($scope.deckName);
         };
     }]);
 })();
