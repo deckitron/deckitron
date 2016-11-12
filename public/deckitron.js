47e4aa68f5bbd029aaddcecaa20d4da399f3d1b6
Matthew-Smith <matthewb.smith88@gmail.com>
2016-11-11 22:04:29 -0500

    starting to use material

--- public/deckitron.js
+++ public/deckitron.js
@@ -1,9 +1,27 @@
-/*global angular*/
+/* global angular*/
 (function () {
     'use strict';
-    const app = angular.module('deckitron', []);
+
+    const app = angular.module('deckitron', ['ngMaterial']);
+
+
+    app.config(($sceDelegateProvider) => {
+          // NOTE: The following config is required because we're
+          //       loading this within JSBin
+          // Allow same origin resource loads.
+          // Allow loading from our assets domain.  Notice the difference
+          // between * and **.
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            'https://rawgit.com/angular/material-start/es5-tutorial/app/assets/svg/**'
+        ]);
+    });
 
     app.controller('DeckitronCore', ['$scope', function ($scope) {
         $scope.title = 'Deckitron';
     }]);
-})();
+
+    app.run(($log) => {
+        $log.debug('Deckitron ready');
+    });
+}());
