71f5492f84ab27fedb9aef05f9c90221f431a381
Matthew-Smith <matthewb.smith88@gmail.com>
2016-11-13 14:30:33 -0500

    remove charm popup

--- public/deckStats.js
+++ public/deckStats.js
@@ -6,15 +6,15 @@
 
     deckStats.controller('deckStats', ['$scope', 'room', '$mdDialog', function ($scope, $room, $mdDialog) {
         $scope.onCharmSelected = function (ev) {
-            const confirm = $mdDialog.alert()
-                 .title('Deck Statistics')
-                 .textContent('Replace this with a custom template with graphs')
-                 .clickOutsideToClose(true)
-                 .ariaLabel('Deck Statistics')
-                 .targetEvent(ev)
-                 .ok('Close');
-
-            $mdDialog.show(confirm);
+            // const confirm = $mdDialog.alert()
+            //      .title('Deck Statistics')
+            //      .textContent('Replace this with a custom template with graphs')
+            //      .clickOutsideToClose(true)
+            //      .ariaLabel('Deck Statistics')
+            //      .targetEvent(ev)
+            //      .ok('Close');
+            //
+            // $mdDialog.show(confirm);
         };
 
         //TODO: listen to the socket and update mana costs when we see something get added or removed
